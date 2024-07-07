909d24efbbd01aab0103d105e4327c30a44cbe59
Kenneth Doerflein <ktd500@verizon.net>
2024-07-06 23:30:32 -0400

    add delay before starting game

--- index.js
+++ index.js
@@ -13,17 +13,10 @@ const slideDelay = 300;
 const animationDelay = slideDelay + flipDelay;
 let dealersCards, dealerTotal, playersCards, playerTotal, gameStatus;
 
-// Force Repaint
-function forceRepaint(element) {
-  element.style.display = "none";
-  element.offsetHeight; // Trigger a reflow
-  element.style.display = "";
-}
-
 // Start the game
-// window.onload = () => {
-//   initializeGame();
-// };
+window.onload = () => {
+  setTimeout(() => initializeGame(), animationDelay);
+};
 
 setupEventListeners();
 
@@ -32,8 +25,6 @@ function initializeGame() {
   clearGameBoard();
   initialDeal();
   setupEventListeners();
-  forceRepaint(playersDiv);
-  forceRepaint(dealersDiv);
 }
 
 function resetGameVariables() {
