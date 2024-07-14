d4c9d9ded264f69e56c1d8adf23982989f475270
Kenneth Doerflein <ktd500@verizon.net>
2024-07-14 03:49:47 -0400

    Add experimental version of split functionality

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // DOM Elements
 const hitBtn = document.getElementById("hitBtn");
+const splitBtn = document.getElementById("splitBtn");
 const standBtn = document.getElementById("standBtn");
 const newGameBtn = document.getElementById("newGameBtn");
 const playersDiv = document.getElementById("playersCards");
 const playersDiv2 = document.getElementById("playersCards2");
 const dealersDiv = document.getElementById("dealersCards");
-const winnerDiv = document.getElementById("winner");
+const messageDiv = document.getElementById("message");
 const pointsDisplay = document.getElementById("pointsDisplay");
 const wagerDisplay = document.getElementById("wagerDisplay");
 const wagerDiv = document.getElementById("wagerDiv");
@@ -23,7 +24,7 @@ const deck = new CardDeck();
 const flipDelay = 700;
 const slideDelay = 300;
 const animationDelay = slideDelay + flipDelay;
-let dealersCards, dealerTotal, playersCards, playerTotal, gameStatus;
+let dealersCards, dealerTotal, playersCards, playerTotal, gameStatus, split, playerHand;
 let playerPoints = 100;
 let currentWager = 0;
 
@@ -31,24 +32,31 @@ let currentWager = 0;
 window.onload = () => {
   let message = document.createElement("h5");
   message.textContent = "Game is loading!";
-  winnerDiv.appendChild(message);
+  messageDiv.appendChild(message);
 
   setTimeout(() => {
     setupEventListeners();
     newGameBtn.toggleAttribute("hidden");
     message.textContent = "Game is ready!";
-  }, animationDelay * 1.5);
+  }, animationDelay * 1);
 };
 
 function initializeGame() {
   if (playerPoints !== 0) {
+    if (playerHand === 1 && split === true) {
+      playersDiv2.toggleAttribute("hidden");
+    }
+
     resetGameVariables();
     clearGameBoard();
-    initialDeal();
     removeEventListeners();
     setupEventListeners();
     updatePoints();
+    toggleWagerElements();
     newGameBtn.textContent = "New Game";
+    let message = document.createElement("h5");
+    message.textContent = "Place your wager to begin!";
+    messageDiv.appendChild(message);
   }
 }
 
@@ -57,13 +65,16 @@ function resetGameVariables() {
   playersCards = [[], []];
   dealerTotal = 0;
   playerTotal = [0, 0];
+  playerHand = 0;
   gameStatus = "inProgress";
+  split = false;
 }
 
 function clearGameBoard() {
   clearDiv(playersDiv);
+  clearDiv(playersDiv2);
   clearDiv(dealersDiv);
-  clearDiv(winnerDiv);
+  clearDiv(messageDiv);
 }
 
 function initialDeal() {
@@ -72,16 +83,25 @@ function initialDeal() {
   setTimeout(() => hit(), animationDelay * 2);
   setTimeout(() => hit("dealer"), animationDelay * 3);
   setTimeout(() => {
-    toggleWagerElements();
+    toggleGameButtons();
     let message = document.createElement("h5");
     message.textContent = "Your Turn!";
-    winnerDiv.appendChild(message);
+    messageDiv.appendChild(message);
+
+    if (playersCards[playerHand].length === 2 && playersCards[playerHand][0].rank === playersCards[playerHand][1].rank && currentWager > 0) {
+      if (currentWager * 2 <= playerPoints) {
+        splitBtn.toggleAttribute("hidden");
+      }
+    }
   }, animationDelay * 3.6);
 }
 
 function toggleGameButtons() {
   hitBtn.toggleAttribute("hidden");
   standBtn.toggleAttribute("hidden");
+  if (!splitBtn.hasAttribute("hidden")) {
+    splitBtn.toggleAttribute("hidden");
+  }
 }
 
 function toggleWagerElements() {
@@ -89,6 +109,8 @@ function toggleWagerElements() {
 }
 
 function removeEventListeners() {
+  hitBtn.removeEventListener("click", hit);
+  splitBtn.removeEventListener("click", splitHand);
   standBtn.removeEventListener("click", endGame);
   newGameBtn.removeEventListener("click", newGame);
   wagerBtn.removeEventListener("click", placeWager);
@@ -100,6 +122,7 @@ function removeEventListeners() {
 
 function setupEventListeners() {
   hitBtn.addEventListener("click", hit);
+  splitBtn.addEventListener("click", splitHand);
   standBtn.addEventListener("click", endGame);
   newGameBtn.addEventListener("click", newGame);
   wagerBtn.addEventListener("click", placeWager);
@@ -173,8 +196,9 @@ function placeWager(event) {
     }
     updatePoints();
     checkStatus("stand");
-    toggleGameButtons();
     toggleWagerElements();
+    initialDeal();
+    clearDiv(messageDiv);
   } else {
     alert("The wager must be a number and greater than 0.");
   }
@@ -187,20 +211,26 @@ function updatePoints() {
 
 async function hit(player = "player") {
   hitBtn.removeEventListener("click", hit);
-  
+
   if (player !== "dealer") {
-    await addCard(playersCards[0], playersDiv, player);
+    let currentWagerPlayerDiv;
+    if (playerHand === 0) {
+      currentWagerPlayerDiv = playersDiv;
+    } else if (playerHand === 1) {
+      currentWagerPlayerDiv = playersDiv2;
+    }
+    await addCard(playersCards[playerHand], currentWagerPlayerDiv, player);
   } else {
     await addCard(dealersCards, dealersDiv, player);
   }
 
-  playerTotal[0] = await calculateTotal(playersCards[0]);
+  playerTotal[playerHand] = await calculateTotal(playersCards[playerHand]);
   dealerTotal = await calculateTotal(dealersCards);
 
   checkStatus("hit");
   setTimeout(() => {
     hitBtn.addEventListener("click", hit);
-  }, animationDelay);
+  }, animationDelay / 1.2);
 }
 
 function addCard(cards, div, player) {
@@ -258,33 +288,73 @@ function calculateTotal(cards) {
 }
 
 function checkStatus(type) {
-  if (playerTotal[0] > 21 || dealerTotal > 21) {
+  if (playerTotal[playerHand] > 21 || dealerTotal > 21) {
     endGame(type);
-  } else if (playerTotal[0] === 21 && standSwitch.checked && currentWager !== 0) {
-    if (playersCards[0].length >= 2 && dealersCards.length >= 2) {
+  } else if (playerTotal[playerHand] === 21 && standSwitch.checked && currentWager !== 0) {
+    if (playersCards[playerHand].length >= 2 && dealersCards.length >= 2) {
       endGame(type);
     }
-  } else if (playersCards[0].length === 2 && playersCards[0][0].rank === playersCards[0][1].rank && currentWager > 0) {
-    let canSplit = confirm("You can Split your hand! Would you like to Split?");
-    if (canSplit) {
-      //splitHand();
+  }
+}
+
+function splitHand() {
+  if (playersCards[0].length === 2 && playersCards[0][0].rank === playersCards[0][1].rank) {
+    toggleGameButtons();
+    splitBtn.toggleAttribute("hidden");
+    split = true;
+
+    playersCards[1].push(playersCards[0].pop());
+
+    clearDiv(playersDiv);
+    playersDiv2.toggleAttribute("hidden");
+
+    for (let i = 0; i < 2; i++) {
+      let imgPath = `./assets/cards-1.3/${playersCards[i][0].image}`;
+      let img = document.createElement("img");
+      img.src = imgPath;
+      img.classList.add("img-fluid", "imgSlide");
+      if (i === 0) {
+        playersDiv.appendChild(img);
+      } else {
+        playersDiv2.appendChild(img);
+      }
     }
+
+    setTimeout(() => hit(), animationDelay * 0.8);
+
+    setTimeout(() => {
+      playerHand = 1;
+      hit();
+    }, animationDelay * 2);
+
+    setTimeout(() => {
+      playerHand = 0;
+      playerTotal[1] = calculateTotal(playersCards[1]);
+      playerTotal[playerHand] = calculateTotal(playersCards[playerHand]);
+      playerPoints -= currentWager;
+      currentWager *= 2;
+      updatePoints();
+      playersDiv.classList.add("activeHand");
+      toggleGameButtons();
+    }, animationDelay * 3);
   }
 }
 
 async function playDealer() {
   while (dealerTotal < 17) {
-    hit("dealer");
+    await hit("dealer");
+    dealerTotal = await calculateTotal(dealersCards);
     await new Promise((resolve) => setTimeout(resolve, animationDelay));
   }
+  endGame("dealer");
 }
 
 function endGame(type) {
-  if (gameStatus === "inProgress") {
-    winnerDiv.removeChild(winnerDiv.firstChild);
+  if (gameStatus === "inProgress" && (playerHand === 1 || split === false)) {
+    messageDiv.removeChild(messageDiv.firstChild);
     let message = document.createElement("h5");
     message.textContent = "Dealer's Turn!";
-    winnerDiv.appendChild(message);
+    messageDiv.appendChild(message);
 
     gameStatus = "gameOver";
     hitBtn.toggleAttribute("hidden");
@@ -292,56 +362,85 @@ function endGame(type) {
     hitBtn.removeEventListener("click", hit);
     standBtn.removeEventListener("click", endGame);
 
+    if (!splitBtn.hasAttribute("hidden")) {
+      splitBtn.toggleAttribute("hidden");
+    }
+
+    if (playerHand === 1 && split === true) {
+      playersDiv2.classList.remove("activeHand");
+    }
+
     let dealerSecondCardImg = dealersDiv.getElementsByTagName("img")[1];
     dealerSecondCardImg.classList.remove("imgSlide");
     let imgPath = `./assets/cards-1.3/${dealersCards[1].image}`;
+
     if (type !== "hit") {
       flipCard(dealerSecondCardImg, imgPath);
     } else {
       setTimeout(() => flipCard(dealerSecondCardImg, imgPath), animationDelay);
     }
+
     const modifiedDelay = type === "hit" ? 2 * animationDelay : animationDelay;
     setTimeout(async () => {
       playerTotal[0] = await calculateTotal(playersCards[0]);
+      playerTotal[1] = await calculateTotal(playersCards[1]);
       dealerTotal = await calculateTotal(dealersCards);
       await playDealer();
-      winnerDiv.removeChild(message);
+      messageDiv.removeChild(message);
       displayWinner();
     }, modifiedDelay);
+  } else if (playerHand === 0 && split === true) {
+    playerHand = 1;
+    playersDiv2.classList.add("activeHand");
+    playersDiv.classList.remove("activeHand");
   }
 }
 
 function displayWinner() {
   let winner = document.createElement("h5");
+  let winner2;
   let finalHandValues = document.createElement("p");
 
-  if (playerTotal[0] > 21) {
-    winner.textContent = "Player Busted, Dealer Wins";
-  } else if (dealerTotal > 21) {
-    winner.textContent = "Dealer Busted, Player Wins";
-    playerPoints += currentWager * 2;
-  } else if (dealerTotal > playerTotal[0]) {
-    winner.textContent = "Dealer Wins";
-  } else if (playerTotal[0] > dealerTotal) {
-    winner.textContent = "Player Wins";
-    playerPoints += currentWager * 2;
-  } else {
-    winner.textContent = "Push (Tie)";
-    playerPoints += currentWager;
+  let outcomes = [[], []];
+
+  for (let handIndex = 0; handIndex < playersCards.length; handIndex++) {
+    let outcome = "";
+    let wagerMultiplier = 0;
+
+    if (playerTotal[handIndex] > 21) {
+      outcome = "Player Busted, Dealer Wins";
+    } else if (dealerTotal > 21) {
+      outcome = "Dealer Busted, Player Wins";
+      wagerMultiplier = 2;
+    } else if (dealerTotal > playerTotal[handIndex]) {
+      outcome = "Dealer Wins";
+    } else if (playerTotal[handIndex] > dealerTotal) {
+      outcome = "Player Wins";
+      wagerMultiplier = 2;
+    } else {
+      outcome = "Push (Tie)";
+      wagerMultiplier = 1;
+    }
+
+    playerPoints += currentWager * wagerMultiplier;
+    outcomes[handIndex] = `Hand ${handIndex + 1}: ${outcome} (Player: ${playerTotal[handIndex]}, Dealer: ${dealerTotal})`;
+
+    winner.textContent = outcomes[0];
+    messageDiv.append(winner);
+
+    if (handIndex === 1 && playersCards[handIndex].length > 0) {
+      winner2 = document.createElement("h5");
+      winner2.textContent = outcomes[1];
+      messageDiv.append(winner2);
+    }
   }
-
-  finalHandValues.textContent = `Final Hand Values: Player - ${playerTotal[0]}, Dealer - ${dealerTotal}`;
-
-  winnerDiv.appendChild(winner);
-  winnerDiv.appendChild(finalHandValues);
 
   currentWager = 0;
   updatePoints();
   if (playerPoints === 0) {
     let message = document.createElement("h5");
     message.textContent = "You are out of points, thank you for playing!";
-    message.classList.add("mt-2");
-    message.classList.add("mb-5");
+    message.classList.add("mt-2", "mb-5");
     bottomDiv.appendChild(message);
   } else {
     newGameBtn.toggleAttribute("hidden");
